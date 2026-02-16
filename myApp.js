3e42fceddde339bb2489095a351f663188ad3add
Angela <angieadrigarcia572@gmail.com>
2026-02-15 23:08:25 -0300

    Change response message and clean up server code
    Updated response message and removed server listening code.

--- myApp.js
+++ myApp.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const app = express();
 const helmet = require('helmet');
+const app = express();
 
 app.use(helmet.hidePoweredBy());
 
 // Esto es para que no se cierre el servidor
 app.get("/", (req, res) => {
-  res.send("¡Servidor funcionando con Helmet!");
+  res.send("Hello Express");
 });
 
 module.exports = app;
 
-// El servidor escuchando
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Tu servidor está corriendo en el puerto ${port}`);
-});
