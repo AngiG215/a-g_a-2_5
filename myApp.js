a5b4c2aa215d664b623069545d2155e7620d439d
Angela <angieadrigarcia572@gmail.com>
2026-02-16 19:31:32 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,17 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-// 1. El reto de freeCodeCamp
+// El corazón del ejercicio:
 app.use(helmet.hidePoweredBy());
 
-// 2. Servir archivos estáticos (necesario para la interfaz)
+// Esto sirve para que la página de freeCodeCamp se vea bonita
 app.use('/public', express.static(process.cwd() + '/public'));
-
-// 3. Ruta principal
 app.get("/", function (request, response) {
   response.sendFile(process.cwd() + '/views/index.html');
 });
 
-// 4. ¡ESTO ES LO QUE ARREGLA EL ERROR!
-// Exportamos la app para que server.js la use
+// ESTA LÍNEA ES LA CLAVE:
 module.exports = app;
