e653e46d4c84684fc6fca932fefb32cc9d16da52
Angela <angieadrigarcia572@gmail.com>
2026-02-15 23:10:26 -0300

    Add server listen functionality to myApp.js

--- myApp.js
+++ myApp.js
@@ -2,12 +2,19 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+// 1. EL MIDDLEWARE DEBE IR PRIMERO
 app.use(helmet.hidePoweredBy());
 
-// Esto es para que no se cierre el servidor
+// 2. RUTA RESPUESTA (A veces el test la busca)
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
+// 3. EXPORTAR EL APP (Fundamental)
 module.exports = app;
 
+// El listen puede quedarse aquí o al final, no afecta si lo anterior está bien
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Tu servidor está corriendo en el puerto ${port}`);
+});
